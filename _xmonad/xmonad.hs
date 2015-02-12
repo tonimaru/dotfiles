d9d1d404e0c821e5f1e22b4d3546cba00a41174a
tonimaru <piro.tonimaru@gmail.com>
2015-02-12 15:04:48 +0900

    layout

--- _xmonad/xmonad.hs
+++ _xmonad/xmonad.hs
@@ -1,25 +1,25 @@
 import XMonad
+import XMonad.Layout.GridVariants
 import XMonad.Layout.NoBorders
+import XMonad.Layout.OneBig
 import XMonad.Layout.ResizableTile
 import XMonad.Util.EZConfig
 
-myLayouts = s ||| m ||| f
-        where s = smartBorders $ ResizableTall 1 (3/100) (2/3) []
-              m = Mirror s
-              f = noBorders Full
-
-myKeys = [ ("M-S-;", spawn "gvim")
-         , ("M-d", sendMessage MirrorShrink)
-         , ("M-u", sendMessage MirrorExpand) ]
-
-myManageHook = composeAll [ title =? "Firefox Preferences" --> doFloat ]
-
+main :: IO ()
 main = xmonad $ defaultConfig
     { borderWidth = 1
     , normalBorderColor = "#222222"
     , focusedBorderColor = "#dddddd"
-    , layoutHook = myLayouts
-    , manageHook = myManageHook
+    , layoutHook = lsl ||| lst ||| lob ||| lfu
+    , manageHook = composeAll [ title =? "Firefox Preferences" --> doFloat ]
     }
     `additionalKeysP` myKeys
-
+    where
+        lsl = smartBorders $ SplitGrid L 1 1 (4/5) (2/1) (3/100)
+        lst = smartBorders $ SplitGrid T 1 1 (4/5) (8/1) (3/100)
+        lfu = noBorders Full
+        lob = smartBorders $ OneBig (4/5) (4/5)
+        myKeys = [ ("M-S-;", spawn "gvim")
+                 , ("M-d", sendMessage MirrorShrink)
+                 , ("M-u", sendMessage MirrorExpand)
+                 ]
