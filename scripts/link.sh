ac76f0779212ce340af55b92039792e7c23db37f
tonimaru <piro.tonimaru@gmail.com>
2015-02-13 13:30:43 +0900

    link

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,16 +1,17 @@
 #/bin/sh
 
-abs_path() {
-    echo $(cd $(dirname $1) && pwd)/$(basename $1)
+abs_dir() {
+    echo $(cd $(dirname $1) && pwd)
 }
 
 to_dst() {
     echo $HOME/$(echo $(basename $1) | sed s/_/./)
 }
 
-current_path="$(cd -P "$(dirname "${BASH_SOURCE:-$0}")" && pwd)"
+current_path="$(abs_dir $0})"
+dotfiles_root_path="$(abs_dir $current_path)"
 
-find $current_path/.. -maxdepth 1 -name "_*" | while read src; do
-    ln -vsfn $(abs_path $src) $(to_dst $src)
+find $dotfiles_root_path -maxdepth 1 -name "_*" | while read src; do
+    ln -vsfn $src $(to_dst $src)
 done
 
