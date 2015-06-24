871522416a749b1f196a6a825388397fcfffc121
tonimaru <piro.tonimaru@gmail.com>
2015-06-24 16:03:18 +0900

    nvim

--- scripts/link.sh
+++ scripts/link.sh
@@ -4,12 +4,19 @@ abs_dir() {
     echo $(cd $(dirname $1) && pwd)
 }
 
+current_path="$(abs_dir $0})"
+dotfiles_root_path="$(abs_dir $current_path)"
+
+# vim, nvim
+
+ln -vsfn $dotfiles_root_path/_vim/vimrc $dotfiles_root_path/_vim/nvimrc
+ln -vsfn $dotfiles_root_path/_vim $dotfiles_root_path/_nvim
+
+# dotfiles/_* -> $HOME/.*
+
 to_dst() {
     echo $HOME/$(echo $(basename $1) | sed s/_/./)
 }
-
-current_path="$(abs_dir $0})"
-dotfiles_root_path="$(abs_dir $current_path)"
 
 find $dotfiles_root_path -maxdepth 1 -name "_*" | while read src; do
     ln -vsfn $src $(to_dst $src)
